73df2bf5709a366f61f65698e1e6954c3753cc0e
Violeta Kukic <vkukic99@gmail.com>
2022-04-12 08:32:51 +0200

    ProjectConfiguraction

---
+++ UserService/Model/Gender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserService.Model
+{
+    public enum Gender
+    {
+        FEMALE, MALE, OTHER
+    }
+}
---
+++ UserService/Model/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UserService.Model
+{
+    public class User : Entity
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public Gender Gender { get; set; }
+        public DateTime BirthDate { get; set; }
+    }
+}
