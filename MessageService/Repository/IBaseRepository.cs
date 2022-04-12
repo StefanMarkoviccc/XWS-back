b647928e51fb4b0fa9cd8fa3f46ab474c9e4197c
bojanalukic99 <lukicb99@gmail.com>
2022-04-12 09:25:26 +0200

    nesto

--- MessageService/Repository/IBaseRepository.cs
+++ MessageService/Repository/IBaseRepository.cs
@@ -1,11 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Threading.Tasks;
 
 namespace MessageService.Repository
 {
-    interface IBaseRepository
+    public interface IBaseRepository<TEntity> where TEntity : class
     {
+        TEntity Get(long id);
+        IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predict);
+        IEnumerable<TEntity> GetAll();
+        IEnumerable<TEntity> Search(string term = "");
+
+        PageResponse<TEntity> GetPage(PageModel model);
+
+        TEntity SingleOrDefault(Expression<Func<TEntity, bool>> predicate);
+
+        void Add(TEntity entity);
+        void Remove(TEntity entity);
+        void RemoveRange(IEnumerable<TEntity> entities);
+        void Update(TEntity entity);
+        void Detach(TEntity entity);
+
+
     }
 }
