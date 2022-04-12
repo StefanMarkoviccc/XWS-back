15714c474c2a2c459cf1bd9347ed119cf129ee96
StefanMarkoviccc <75496612+StefanMarkoviccc@users.noreply.github.com>
2022-04-12 08:39:01 +0200

    job service model

---
+++ JobService/Model/CreateJob.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobService.Model
+{
+    public class CreateJob : Entity
+    {
+       public String JobDescription { get; set; }
+       public String JobPosition { get; set; }
+       public String ActivityLog { get; set; }
+       public String JobConditions { get; set; }
+    }
+}
