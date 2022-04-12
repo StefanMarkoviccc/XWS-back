f4c2820bd3e0e2671e4c1728946882aaf558ab70
StefanMarkoviccc <75496612+StefanMarkoviccc@users.noreply.github.com>
2022-04-12 08:44:17 +0200

    job service configuration

--- JobService/Configuration/ProjectConfiguration.cs
+++ JobService/Configuration/ProjectConfiguration.cs
@@ -1,11 +1,43 @@
-﻿using System;
+﻿using JobService.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace JobService.Configuration
+namespace MessageService.Configuration
 {
-    public class ProjectConfiguration
+
+    public class ProjectConfiguration : IProjectConfiguration
     {
+        public string FrontendURL { get; set; }
+        public string ClientID { get; set; }
+        public string CLientSecret { get; set; }
+        public DatabaseConfiguration DatabaseConfiguration { get; set; } = new DatabaseConfiguration();
+        public EmailSettings EmailSettings { get; set; } = new EmailSettings();
+
+        public Jwt Jwt { get; set; } = new Jwt();
+    }
+
+    public class DatabaseConfiguration
+    {
+        public string ConnectionString { get; set; }
+    }
+
+    public class EmailSettings
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public int Port { get; set; }
+        public string DisplayName { get; set; }
+        public string ServerAddress { get; set; }
+        public bool EnableSsl { get; set; }
+    }
+
+    public class Jwt
+    {
+        public string Key { get; set; }
+        public string Issuer { get; set; }
+        public string Audience { get; set; }
+        public string Subject { get; set; }
     }
 }
