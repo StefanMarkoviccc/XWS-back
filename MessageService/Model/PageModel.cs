404cfd56d18276f1a74244c8018f863b94112adf
bojanalukic99 <lukicb99@gmail.com>
2022-04-12 09:30:45 +0200

    page model

--- MessageService/Model/PageModel.cs
+++ MessageService/Model/PageModel.cs
@@ -7,5 +7,20 @@ namespace MessageService.Model
 {
     public class PageModel
     {
+        public PageModel()
+        {
+
+        }
+
+        public PageModel(int page, int perPage, string search)
+        {
+            Page = page;
+            PerPage = perPage;
+            Search = search;
+        }
+
+        public int Page { get; set; }
+        public int PerPage { get; set; } = 30;
+        public string Search { get; set; } = string.Empty;
     }
 }
