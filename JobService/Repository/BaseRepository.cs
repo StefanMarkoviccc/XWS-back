cd29bd86593c1d19bcd17557fbab42615d21650b
StefanMarkoviccc <75496612+StefanMarkoviccc@users.noreply.github.com>
2022-04-14 07:30:04 +0200

    update

--- JobService/Repository/BaseRepository.cs
+++ JobService/Repository/BaseRepository.cs
@@ -1,33 +1,32 @@
-﻿using MessageService.Model;
+﻿using JobService.Model;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Threading.Tasks;
 
-namespace MessageService.Repository
+namespace JobService.Repository
 {
     public class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
     {
         private readonly DbContext _context;
 
-        public MessageContext LogicalContractContext 
+        public JobContext LogicalContractContext
         {
-            get { return _context as MessageContext; }
+            get { return _context as JobContext; }
         }
 
-        public BaseRepository(DbContext context) 
+        public BaseRepository(DbContext context)
         {
             _context = context;
         }
 
-        public virtual TEntity Get(long id) 
+        public virtual TEntity Get(long id)
         {
             return _context.Set<TEntity>().Find(id);
         }
 
-        public virtual IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate) 
+        public virtual IEnumerable<TEntity> Find(Expression<Func<TEntity, bool>> predicate)
         {
             return _context.Set<TEntity>().Where(predicate);
         }
@@ -37,43 +36,43 @@ namespace MessageService.Repository
             return _context.Set<TEntity>().SingleOrDefault(predicate);
         }
 
-        public virtual void Add(TEntity entity) 
+        public virtual void Add(TEntity entity)
         {
             _context.Set<TEntity>().Add(entity);
         }
 
-        public virtual void AddRange(IEnumerable<TEntity> entities) 
+        public virtual void AddRange(IEnumerable<TEntity> entities)
         {
             _context.Set<TEntity>().AddRange(entities);
         }
 
-        public virtual void Remove(TEntity entity) 
+        public virtual void Remove(TEntity entity)
         {
             _context.Set<TEntity>().Remove(entity);
         }
 
-        public virtual void RemoveRange(IEnumerable<TEntity> entities) 
+        public virtual void RemoveRange(IEnumerable<TEntity> entities)
         {
             _context.Set<TEntity>().RemoveRange(entities);
         }
 
-        public virtual void Update(TEntity entity) 
+        public virtual void Update(TEntity entity)
         {
             _context.Set<TEntity>().Attach(entity);
             _context.Entry(entity).State = EntityState.Modified;
         }
 
-        public virtual void Detach(TEntity entity) 
+        public virtual void Detach(TEntity entity)
         {
             _context.Entry(entity).State = EntityState.Detached;
         }
 
-        public virtual IEnumerable<TEntity> GetAll() 
+        public virtual IEnumerable<TEntity> GetAll()
         {
             return _context.Set<TEntity>().Where(x => !(x as Entity).Deleted).ToList();
         }
 
-        public virtual IEnumerable<TEntity> Search(string term = "") 
+        public virtual IEnumerable<TEntity> Search(string term = "")
         {
             throw new NotImplementedException();
         }
--- MessageService/Model/MessageContext.cs
+++ MessageService/Model/MessageContext.cs
@@ -5,7 +5,7 @@ using System.Threading.Tasks;
 
 namespace MessageService.Model
 {
-    public interface MessageContext
+    public class MessageContext
     {
     }
 }
