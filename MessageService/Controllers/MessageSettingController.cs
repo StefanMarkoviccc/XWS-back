bea9ccdf48fceacbc1ea707d860f10385aa1b596
bojanalukic99 <lukicb99@gmail.com>
2022-04-14 09:49:02 +0200

    message settings controller

--- MessageService/Controllers/MessageSettingController.cs
+++ MessageService/Controllers/MessageSettingController.cs
@@ -1,11 +1,21 @@
-﻿using System;
+﻿using MessageService.Configuration;
+using MessageService.Model;
+using MessageService.Service;
+using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MessageService.Controllers
 {
-    public class MessageSettingController
+    [ApiController]
+    [Route("[controller]")]
+    public class MessageSettingController : BaseController<MessageSettings>
     {
+        private IMessageSettingsService messageSettingsService;
+        public MessageSettingController(ProjectConfiguration projectConfiguration, IUserService userService, IMessageSettingsService messageSettingsService) : base(projectConfiguration, userService) {
+            this.messageSettingsService = messageSettingsService;
+        }
     }
 }
