62ee74934ea2411d74cdee5f44e6097b27f38480
JelenaPuric <puricjelena99@gmail.com>
2022-04-14 09:09:12 +0200

    post service

---
+++ PostService/Model/PostContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PostService.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostService.Model
+{
+    public class PostContext : DbContext
+    {
+        private static ProjectConfiguration _configuration;
+        public PostContext() { }
+
+        public PostContext(DbContextOptions<PostContext> options, ProjectConfiguration configuration) : base(options)
+        {
+            if (configuration != null)
+            {
+                _configuration = configuration;
+            }
+        }
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (optionsBuilder.IsConfigured)
+            {
+                return;
+            }
+
+            optionsBuilder.UseSqlServer(_configuration.DatabaseConfiguration.ConnectionString);
+        }
+    }
+        
+}
+
---
+++ PostService/Repository/PostRepository.cs
@@ -0,0 +1,13 @@
+﻿using PostService.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PostService.Repository
+{
+    public class PostRepository : BaseRepository<Post>
+    {
+        public PostRepository(PostContext context) : base(context) { }
+    }
+}
