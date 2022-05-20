15adbe372ff08f36bdca4731aeb745be6fd35b9d
StefanMarkoviccc <75496612+StefanMarkoviccc@users.noreply.github.com>
2022-05-20 09:19:50 +0200

    no sql base logger service

---
+++ LoggerService/Model/Log.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace LoggerService.Model
+{
+    public class Log
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string? Id { get; set; }
+
+        [BsonElement("User")]
+        public string User { get; set; } = null!;
+
+        [BsonElement("LogText")]
+        public string LogText { get; set; } = null!;
+
+        [BsonElement("Service")]
+        public string Service { get; set; } = null!;
+    }
+}
---
+++ LoggerService/Services/LogService.cs
@@ -0,0 +1,44 @@
+﻿using LoggerService.Configuration;
+using LoggerService.Model;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LoggerService.Services
+{
+    public class LogService
+    {
+        private readonly IMongoCollection<Log> _logCollection;
+
+        public LogService(
+        IOptions<ProjectConfiguration> projectConfiguration)
+        {
+            var mongoClient = new MongoClient(
+                projectConfiguration.Value.Mongo.ConnectionString);
+
+            var mongoDatabase = mongoClient.GetDatabase(
+                projectConfiguration.Value.Mongo.DatabaseName);
+
+            _logCollection = mongoDatabase.GetCollection<Log>(
+                projectConfiguration.Value.Mongo.BooksCollectionName);
+        }
+
+        public async Task<List<Log>> GetAsync() =>
+        await _logCollection.Find(_ => true).ToListAsync();
+
+        public async Task<Log?> GetAsync(string id) =>
+            await _logCollection.Find(x => x.Id == id).FirstOrDefaultAsync();
+
+        public async Task CreateAsync(Log log) =>
+            await _logCollection.InsertOneAsync(log);
+
+        public async Task UpdateAsync(string id, Log log) =>
+            await _logCollection.ReplaceOneAsync(x => x.Id == id, log);
+
+        public async Task RemoveAsync(string id) =>
+            await _logCollection.DeleteOneAsync(x => x.Id == id);
+    }
+}
